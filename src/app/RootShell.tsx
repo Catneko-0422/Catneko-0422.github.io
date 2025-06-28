061e5fbd0c15645c17205f108a4b787c6f6a9f20
Nekocat <linyian0422@gmail.com>
2025-06-28 13:22:12 +0800

    fix: add missing React imports

--- src/app/RootShell.tsx
+++ src/app/RootShell.tsx
@@ -2,6 +2,7 @@
 
 import { Geist, Geist_Mono } from "next/font/google";
 import Link from "next/link";
+import type React from "react";
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
